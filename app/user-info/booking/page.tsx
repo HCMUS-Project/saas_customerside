e68d9c5bb7dddbdc1a3ac5d6959c6572f4ed8f97
HiusHa1 <hahuynhduchuy@gmail.com>
2024-03-25 20:58:50 +0700

    add booking page

--- app/user-info/booking/page.tsx
+++ app/user-info/booking/page.tsx
@@ -12,8 +12,21 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Booking, columns } from "./columns";
+import { DataTable } from "./data-table";
 
-const userInfo = () => {
+async function getData(): Promise<Booking[]> {
+  const res = await fetch(
+    "https://6577fda6197926adf62f397c.mockapi.io/product"
+  );
+  const data = await res.json();
+  // Fetch data from your API here.
+  return data;
+  // ...
+}
+
+export default async function Booking() {
+  const data = await getData();
   return (
     <div className="py-6">
       <div className="mt-6 flex justify-center  ">
@@ -36,19 +49,19 @@ const userInfo = () => {
             data-te-ripple-color="light"
             className={cn(
               "inline-block rounded-md px-6 pb-3 pt-3.5 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-100 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700",
-              "border-b-[3px] border-blue-300"
+              "border-b-[3px] "
             )}
           >
             Account
           </Link>
 
           <Link
+            href="/user-info"
             data-te-ripple-init
             data-te-ripple-color="light"
-            href="user-info/booking"
             className={cn(
               "inline-block rounded-md px-6 pb-3 pt-3.5 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-100 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700",
-              ""
+              "border-b-[3px] border-blue-300"
             )}
           >
             Booking
@@ -68,77 +81,10 @@ const userInfo = () => {
         </div>
       </div>
       <div className="py-6">
-        <Card>
-          <CardHeader>
-            <div className="flex justify-between items-center">
-              <div>
-                <h1>Profile</h1>
-                <p className="text-xs">
-                  basic info for faster booking experience
-                </p>
-              </div>
-              <Button variant="link">edit profile</Button>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-3 gap-x-0 py-2">
-              <div>Name</div>
-              <div>Huy Ha</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-3 py-2">
-              <div>Email</div>
-              <div>Huy Ha</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-3 py-2">
-              <div>Phone Number</div>
-              <div> Huy Ha</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-3 py-2">
-              <div>Address</div>
-              <div> Huy Ha</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-3 py-2">
-              <div>Date of birth</div>
-              <div> Huy Ha</div>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-      <div>
-        <Card>
-          <CardHeader>
-            <h1>Login Details</h1>
-            <p className="text-xs">manage your email and password address</p>
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-3 py-2">
-              <div>Mobile Number</div>
-              <div>Huy Ha </div>
-            </div>
-
-            <hr />
-            <div className="grid grid-cols-3 py-2  justify-between items-center">
-              <div>Email ID</div>
-              <div> Huy Ha</div>
-            </div>
-            <hr />
-            <div className="grid grid-cols-3 py-2  ">
-              <div>Password</div>
-              <div> Huy Ha</div>
-
-              <Button className="justify-end" variant="link">
-                change password
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={data} />
+        </div>
       </div>
     </div>
   );
-};
-
-export default userInfo;
+}
